d1521c4503806b032134c4eb5e3771ea15cd3989
hezronokwach <hezronokwach@gmail.com>
2024-12-05 13:12:16 +0300

    Test the gin framework

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "authorization/initializers"
+import (
+	"authorization/initializers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func init() {
 	initializers.LoadEnv()
-
 }
-func main(){
-	
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
