3f27544e5889b68373d9d32441b4746871ee2f0f
hokwach <hezronokwach@gmail.com>
2024-12-05 21:36:27 +0300

    Add cmd messages for database setup and initialization

--- initializers/connectToDb.go
+++ initializers/connectToDb.go
@@ -3,16 +3,26 @@ package initializers
 import (
 	"log"
 
-	"gorm.io/driver/sqlite" // Correct import for the SQLite driver
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB // Declare DB as *gorm.DB
+var DB *gorm.DB
 
 func ConnectToDb() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("./app.db"), &gorm.Config{}) // Use gorm.Open
+	dbPath := "./db2.db"
+
+	// Delete the existing database file
+	// if err := os.Remove(dbPath); err != nil && !os.IsNotExist(err) {
+	//     log.Printf("Error removing existing database: %v", err)
+	// }
+
+	// Create fresh database connection
+	DB, err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
+
+	log.Println("Successfully connected to fresh database")
 }
