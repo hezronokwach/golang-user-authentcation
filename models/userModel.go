80cb7bb2a18e04c8ae43746e368a7ee7bd602384
hokwach <hezronokwach@gmail.com>
2024-12-05 21:37:49 +0300

    Add properties to email and password field

--- models/userModel.go
+++ models/userModel.go
@@ -2,8 +2,9 @@ package models
 
 import "gorm.io/gorm"
 
-type User struct{
-	gorm.Model
-	Password string
-	Email string `gorm:"unique"`
+// models/user.go
+type User struct {
+    gorm.Model
+    Email    string `gorm:"uniqueIndex;not null"`
+    Password string `gorm:"not null"`
 }
